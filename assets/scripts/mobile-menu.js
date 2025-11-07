55dd09eb3081b1c150672e0512f1188f5aef0c8a
Giulliano Guimarães <llthguimaraes@gmail.com>
2025-11-07 10:02:51 -0300

    refatoração: seção sobre

--- assets/scripts/mobile-menu.js
+++ assets/scripts/mobile-menu.js
@@ -4,6 +4,7 @@ import { isVisible, transitionGap } from "./variables.js"
 const menuIcon = document.querySelector(".j_menu_icon")
 const menu = document.querySelector(".j_menu")
 const menuOverlay = document.querySelector(".j_menu_overlay")
+const menuLinks = Array.from(menuOverlay.querySelectorAll("a"))
 
 const closeMenu = () => {
     menu.classList.remove("menu-open")
@@ -27,6 +28,14 @@ const MobileMenu = () => {
         if (target.classList.contains("j_close")) {
             closeMenu()
         }
+    })
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            if (window.innerWidth <= 992) {
+                closeMenu()
+            }
+        })
     })
 }
 
