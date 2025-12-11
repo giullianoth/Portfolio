318900f2505126b74a886104a091de49d7afc994
Giulliano Guimarães <llthguimaraes@gmail.com>
2025-12-11 09:26:29 -0300

    cards do portfólio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,14 +3,12 @@ import AddCopyrightYear from "./copyright-year.js"
 import { HeaderBehavior } from "./header.js"
 import MobileMenu from "./mobile-menu.js"
 import Modal from "./modal.js"
-import PortfolioImagePreview from "./portfolio.js"
 
 MobileMenu()
 HeaderBehavior()
 Carousel()
 AddCopyrightYear()
 Modal()
-PortfolioImagePreview()
 
 window.addEventListener("scroll", () => {
     HeaderBehavior()
@@ -18,5 +16,4 @@ window.addEventListener("scroll", () => {
 
 window.addEventListener("resize", () => {
     Carousel()
-    PortfolioImagePreview()
 })
